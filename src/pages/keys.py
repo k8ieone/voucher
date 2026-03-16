c110aa9ce85d110d684c818c55a108df29e7cd9e
k8ie <k8ie@mcld.eu>
2026-03-16 08:25:14 +0100

    Initial seed backup concept

--- src/pages/keys.py
+++ src/pages/keys.py
@@ -5,6 +5,15 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 
+# from
+
 @Gtk.Template(resource_path='/one/k8ie/Voucher/pages/keys.ui')
 class VoucherKeysTab(Adw.NavigationPage):
     __gtype_name__ = 'VoucherKeysTab'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @Gtk.Template.Callback()
+    def on_show(self, widget):
+        print(self.get_root().settings)
